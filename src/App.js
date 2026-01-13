3bd80e637de69a76d1483633b03ffa99c2dda30f
wasayrajput <wasayrajput2450786123@gmail.com>
2026-01-13 21:58:30 +0500

    Initial commit: React Calculator with basic operations

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Calculator from './components/Calculator/Calculator';
+
+function App() {
+  return (
+    <div>
+      <Calculator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import './Calculator.css'
+
+const Calculator = () => {
+
+    const [calculate, setCalculate] = useState('');
+
+    const ClickButton = (value) => {
+        if (value === 'AC') {
+            setCalculate('')
+        }
+        else if (value === 'DC') {
+            setCalculate(calculate.slice(0, -1));
+        }
+        else if (value === '=') {
+            try {
+                setCalculate(eval(calculate).toString());
+
+            } 
+            catch (error) {
+                setCalculate('Error');
+            }
+        }
+
+        else{
+            setCalculate(calculate + value);
+        }
+    };
+
+  return (
+    <div className='container'>
+        <div className='calcu'>
+           <div>
+            <input type='text' placeholder='0' className='display' value={calculate}></input>
+           </div>
+
+           <div className='buttons'>
+            <div className='btn'>
+                <button onClick={() => ClickButton('AC')}>AC</button>
+                <button onClick={() => ClickButton('DC')}>DC</button>
+                <button onClick={() => ClickButton('.')}>.</button>
+                <button onClick={() => ClickButton('/')}>/</button>
+            </div>
+            <div className='btn'>
+                <button onClick={() => ClickButton('7')}>7</button>
+                <button onClick={() => ClickButton('8')}>8</button>
+                <button onClick={() => ClickButton('9')}>9</button>
+                <button onClick={() => ClickButton('*')}>X</button>
+            </div>
+            <div className='btn'>
+                <button onClick={() => ClickButton('4')}>4</button>
+                <button onClick={() => ClickButton('5')}>5</button>
+                <button onClick={() => ClickButton('6')}>6</button>
+                <button onClick={() => ClickButton('+')}>+</button>
+            </div>
+            <div className='btn'>
+                <button onClick={() => ClickButton('1')}>1</button>
+                <button onClick={() => ClickButton('2')}>2</button>
+                <button onClick={() => ClickButton('3')}>3</button>
+                <button onClick={() => ClickButton('-')}>-</button>
+            </div >
+            <div className='btn btnn'> 
+                <button onClick={() => ClickButton('0')}>0</button>
+                <button id='btnn' onClick={() => ClickButton('=')}>=</button>
+            </div>
+           </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Calculator
