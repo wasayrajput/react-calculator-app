5a6293b97bc1193b2e2fabca99cb7bbe85ea1d11
wasayrajput <wasayrajput2450786123@gmail.com>
2026-09-10 01:53:11 +0530

    fix: ignore eslint no-eval rule

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -14,6 +14,7 @@ const Calculator = () => {
         }
         else if (value === '=') {
             try {
+                // eslint-disable-next-line no-eval
                 setCalculate(eval(calculate).toString());
 
             } 
@@ -30,40 +31,40 @@ const Calculator = () => {
   return (
     <div className='container'>
         <div className='calcu'>
-           <div>
-            <input type='text' placeholder='0' className='display' value={calculate}></input>
-           </div>
+            <div>
+             <input type='text' placeholder='0' className='display' value={calculate}></input>
+            </div>
 
-           <div className='buttons'>
-            <div className='btn'>
-                <button onClick={() => ClickButton('AC')}>AC</button>
-                <button onClick={() => ClickButton('DC')}>DC</button>
-                <button onClick={() => ClickButton('.')}>.</button>
-                <button onClick={() => ClickButton('/')}>/</button>
+            <div className='buttons'>
+             <div className='btn'>
+                 <button onClick={() => ClickButton('AC')}>AC</button>
+                 <button onClick={() => ClickButton('DC')}>DC</button>
+                 <button onClick={() => ClickButton('.')}>.</button>
+                 <button onClick={() => ClickButton('/')}>/</button>
+             </div>
+             <div className='btn'>
+                 <button onClick={() => ClickButton('7')}>7</button>
+                 <button onClick={() => ClickButton('8')}>8</button>
+                 <button onClick={() => ClickButton('9')}>9</button>
+                 <button onClick={() => ClickButton('*')}>X</button>
+             </div>
+             <div className='btn'>
+                 <button onClick={() => ClickButton('4')}>4</button>
+                 <button onClick={() => ClickButton('5')}>5</button>
+                 <button onClick={() => ClickButton('6')}>6</button>
+                 <button onClick={() => ClickButton('+')}>+</button>
+             </div>
+             <div className='btn'>
+                 <button onClick={() => ClickButton('1')}>1</button>
+                 <button onClick={() => ClickButton('2')}>2</button>
+                 <button onClick={() => ClickButton('3')}>3</button>
+                 <button onClick={() => ClickButton('-')}>-</button>
+             </div >
+             <div className='btn btnn'> 
+                 <button onClick={() => ClickButton('0')}>0</button>
+                 <button id='btnn' onClick={() => ClickButton('=')}>=</button>
+             </div>
             </div>
-            <div className='btn'>
-                <button onClick={() => ClickButton('7')}>7</button>
-                <button onClick={() => ClickButton('8')}>8</button>
-                <button onClick={() => ClickButton('9')}>9</button>
-                <button onClick={() => ClickButton('*')}>X</button>
-            </div>
-            <div className='btn'>
-                <button onClick={() => ClickButton('4')}>4</button>
-                <button onClick={() => ClickButton('5')}>5</button>
-                <button onClick={() => ClickButton('6')}>6</button>
-                <button onClick={() => ClickButton('+')}>+</button>
-            </div>
-            <div className='btn'>
-                <button onClick={() => ClickButton('1')}>1</button>
-                <button onClick={() => ClickButton('2')}>2</button>
-                <button onClick={() => ClickButton('3')}>3</button>
-                <button onClick={() => ClickButton('-')}>-</button>
-            </div >
-            <div className='btn btnn'> 
-                <button onClick={() => ClickButton('0')}>0</button>
-                <button id='btnn' onClick={() => ClickButton('=')}>=</button>
-            </div>
-           </div>
         </div>
 
     </div>
